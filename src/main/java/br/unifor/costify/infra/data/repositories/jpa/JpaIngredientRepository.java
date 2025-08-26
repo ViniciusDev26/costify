d0b13eb3be5eca4be162c01be8b2c3b529f65740
Carlos Vinicius <viniciusdev.26@gmail.com>
2025-08-26 20:48:00 -0300

    🔧 refactor ingredient repository methods
    Description: This commit refactors the `findById` method in the `PostgresIngredientRepository` to remove the final modifier and cleans up the `JpaIngredientRepository` by removing the unnecessary `@Component` annotation.
    
    Changelog:
    - Removed final modifier from `findById` method
    - Removed `@Component` annotation from `JpaIngredientRepository`

--- src/main/java/br/unifor/costify/infra/data/repositories/jpa/JpaIngredientRepository.java
+++ src/main/java/br/unifor/costify/infra/data/repositories/jpa/JpaIngredientRepository.java
@@ -2,9 +2,7 @@ package br.unifor.costify.infra.data.repositories.jpa;
 
 import br.unifor.costify.infra.data.entities.IngredientTable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
-@Component
 public interface JpaIngredientRepository extends JpaRepository<IngredientTable, String> {
   boolean existsByName(String name);
 }
